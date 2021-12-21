83c686ebaf2396eb0df2681d37a5c41cf85c1efa
✫ MR USHAN ♔ <89759475+Mrushan2021@users.noreply.github.com>
2021-12-21 15:13:27 +0530

    Update heroku_deploy.sh

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,5 +1,5 @@
 heroku create
-heroku buildpacks:add --index 1 https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git
+heroku buildpacks:add --index 1 https://github.com/Mrushan2021/heroku-buildpack-ffmpeg-latest.git
 heroku buildpacks:add heroku/python
 heroku config:set $(cat .env)
 git push heroku master
